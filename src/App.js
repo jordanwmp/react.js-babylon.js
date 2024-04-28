9dae2e3f69db52de3f5626d5a53ef1dd332a3510
Jordan Willian <jordanwillian.ecp@gmail.com>
2024-04-28 13:16:33 -0300

    update scente to be a component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useEffect } from 'react';
 
-import createScene from './components/scene'
+import CreateScene from './components/scene'
 
 import './App.css';
 
 function App() {
 
-  useEffect(() => {
-    const canvas = document.getElementById('renderCanvas');
-    console.log('canvas ', canvas);
-    createScene(canvas, window);
-  }, [])
-
-
   return (
     <div className="App">
-      <canvas id='renderCanvas'></canvas>
+      <CreateScene />
     </div>
   );
 
--- src/components/scene.js
+++ src/components/scene.js
@@ -1,30 +1,37 @@
+import react, { useEffect, useRef } from 'react';
 import * as BABYLON from '@babylonjs/core';
 
-const createScene = (canvas, window) => {
+const CreateScene = () => {
 
-    const engine = new BABYLON.Engine(canvas);
-    const scene = new BABYLON.Scene(engine, true);
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
-    camera.attachControl(canvas, true);
+    useEffect(() => {
+        const canvas = document.getElementById('renderCanvas');
+        const engine = new BABYLON.Engine(canvas);
+        const scene = new BABYLON.Scene(engine, true);
+        const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
+        camera.attachControl(canvas, true);
 
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-    const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+        const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 
-    engine.runRenderLoop(() => {
-        scene.render()
-    })
+        engine.runRenderLoop(() => {
+            scene.render()
+        })
 
-    onResize(window, scene)
+        onResize(scene)
 
-    return scene;
+    }, [])
+
+    return <canvas id='renderCanvas'></canvas>
+
+    //return scene;
 }
 
-const onResize = (window, scene) => {
+const onResize = (scene) => {
     window.addEventListener('resize', () => {
         console.log('resize event trigger')
         scene.getEngine().resize()
     })
 }
 
-export default createScene;
+export default CreateScene;
